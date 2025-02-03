a47b16e27de4a026f32788c27c59c1efe9919a23
Jair Horeb Jiménez Garcia <jair.jimenez8050@alumnos.udg.mx>
2025-02-02 23:22:11 -0600

    Comentarios

--- Algoritmo_greedy.py
+++ Algoritmo_greedy.py
@@ -6,13 +6,13 @@
 
 def dar_cambio(monto):
     billetes_monedas = [1000, 500, 200, 100, 50, 20, 10, 5, 2, 1]  # Billetes y monedas 
-    cambio = {}
+    cambio = {} 
     
     for valor in billetes_monedas:
-        if monto >= valor:
-            cantidad = monto // valor
-            cambio[valor] = cantidad
-            monto -= cantidad * valor
+        if monto >= valor: # Si el monto es mayor o igual al valor de la moneda
+            cantidad = monto // valor # Calcula la cantidad de monedas que se pueden entregar
+            cambio[valor] = cantidad  # Agrega la moneda y su cantidad 
+            monto -= cantidad * valor # Resta el valor de las monedas entregadas 
     
     return cambio
 
@@ -21,6 +21,6 @@ monto = int(input("Cuanto peso nenecitamos? "))
 cambio = dar_cambio(monto)
 
 print("Morralla dada:")
-for valor, cantidad in cambio.items():
-    tipo = "billete" if valor >= 20 else "moneda"
-    print(f"{cantidad} {tipo}(s) de ${valor} MXN")
+for valor, cantidad in cambio.items(): # Imprime el cambio
+    tipo = "billete" if valor >= 20 else "moneda" # Determina si es un billete o moneda
+    print(f"{cantidad} {tipo}(s) de ${valor} MXN") 
