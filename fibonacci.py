86253f5e6ef7ba6c7a6d7f0d2084ba74d86de850
Alexis212004 <alexis.gonzalez6749@alumnos.udg.mx>
2025-02-08 19:51:08 -0600

    Update fibonacci.py
    yaquedo

--- fibonacci.py
+++ fibonacci.py
@@ -1,10 +1,21 @@
+"""
+° Alexis González Reynoso
+° Jair horeb Jiménez García
+° Eliezer Mora González
+"""
+
+
 def papifibonacci(primer, segundo, limite, suma=0):
-    if primer >= limite:  # Caso base
-        print("\nSuma total:", suma)  # Imprimir la suma al final
+    # Caso base: si el número actual es mayor o igual al límite, finaliza.
+    if primer >= limite:
+        print("\nSuma total:", suma)
         return
 
-    print(primer, end=" ")  # Imprimir el número actual
-    papifibonacci(segundo, primer + segundo, limite, suma + primer)  # Llamada recursiva
+    # Imprime el número actual y llama recursivamente con los siguientes valores.
+    print(primer, end=" ")
+    papifibonacci(segundo, primer + segundo, limite, suma + primer)
 
-papifibonacci(0,1,0,20)
+#llamamos la funcion con sus argumentos
+papifibonacci(0, 1, 45)
+
 
