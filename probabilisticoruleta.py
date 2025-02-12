a803cd66621b7fa43cb2f5e9bb96924fe4a8c778
Ezzzzzer <123517934+Ezzzzzer@users.noreply.github.com>
2025-02-11 19:03:36 -0600

    algo propabilistico

---
+++ probabilisticoruleta.py
@@ -0,0 +1,8 @@
+import random
+import os
+import webbrowser
+
+if random.randint(0,1) == 1:
+    webbrowser.open("https://i.pinimg.com/736x/34/5c/9c/345c9c23f8ac9ba092aa634edea5b83f.jpg")
+else:
+   webbrowser.open("https://pm1.aminoapps.com/7849/697d637b2664d9f784234db07d48f30d28db7210r1-1280-720v2_00.jpg" )
